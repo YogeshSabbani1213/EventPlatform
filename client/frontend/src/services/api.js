800dc18bc31692f3840ea48f91458ee3bce73fe4
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2025-12-30 22:41:53 +0530

    Updated api

--- client/frontend/src/services/api.js
+++ client/frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5005/api";
+// api.js
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5005/api";
 
  
 // later replace with deployed backend URL
